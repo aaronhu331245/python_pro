4ed0c3796c00d7e89f06a50ceb1d4ecb49405950
aaronhu331245 <aaronhu@outlook.it>
2025-04-28 17:13:52 +0200

    Create main.py
    Prima applicazione

---
+++ main.py
@@ -0,0 +1,13 @@
+import random
+
+parole = ["Ciao!", "Python Pro", "Kodland"] 
+print(parole[1])
+
+nome = input("come ti chiami?")
+print("Ciao ", nome)
+
+emojis = ["^^", "0_o", ":)", "\_(0_0)_/¯", "(￢_￢)"]
+print(random.choice(emojis))
+
+IO = ["personalizzare il PC", "suonare il piano", "fare origami"]
+print("sono bravo a ", random.choice(IO))
